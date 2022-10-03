d2cd2a0fc0289405f89db2a908e01792da129488
suraj235 <suraj@krenovate.com>
2022-10-03 13:47:10 +0530

    first commit

---
+++ scripts/common.js
@@ -0,0 +1,32 @@
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    // loop through a collection of all HTML elements
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+            // make an HTTP request using the attribute value as the file name
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+
+            return;
+        }
+    }
+};
+
+// Call to the main function
+includeHTML();
